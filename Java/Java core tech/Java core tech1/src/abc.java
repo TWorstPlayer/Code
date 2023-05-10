067e0106b83fd36508436008143eca061ea29ffa
@TheWorstPlayer <1339653485@qq.com>
2023-05-10 18:29:25 +0800

    1

---
+++ Java/Java core tech/Java core tech1/src/abc.java
@@ -0,0 +1,6 @@
+public class abc extends abstract1{
+    int a=1;
+    public void printf1(){
+        System.out.println("adasdasdas");
+    }
+}
---
+++ Java/Java core tech/Java core tech1/src/abstract1.java
@@ -0,0 +1,6 @@
+public class abstract1 {
+    int a=0;
+    public void printf1(){
+        System.out.println("abc");
+    }
+}
